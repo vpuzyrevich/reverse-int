4095ba3af95e5b14098ad82732333ace99a3e0ec
vpuzyrevich <v.puzyrevich@gmail.com>
2022-02-04 20:52:53 +0300

    feat: add function revefse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    const num = String(Math.abs(n));
+    let result = '';
+    for(let i = 0; i < num.length; i++) {
+       result = `${num[i]}${result}`;
+    }
+    return +result;
+};
